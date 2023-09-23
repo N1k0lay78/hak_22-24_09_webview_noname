585afbf8082dba9c330cf131152e7029148cab6a
Rjkzavr <kolya.toropof@gmail.com>
2023-09-23 10:36:43 +0300

    make check images with AI bug compress images send blue bug download images at mobile version

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import io
-
 from flask import Flask, render_template, request, json, jsonify
 import config
 import urllib.request
 from PIL import Image
 from base64 import encodebytes
+from ultralytics import YOLO
+import numpy as np
+import cv2
 
 application = Flask(__name__)
 application.config.from_object(config)
@@ -17,50 +19,68 @@ def promotional_page():
     return render_template('base.html', title="Обработка изображения", is_desktop=is_desktop)
 
 
-def answer(weight_path:str, img_path: str) -> str:
-    try:
-        from ultralytics import YOLO
-    except:
-        return "No module YOLO! YOLO is not installed!"
-
-    try:
-        from PIL import Image
-    except:
-        return "No module Image! Image is not installed!"
-
-    try:
-        import numpy as np
-    except:
-        return "No module numpy! numpy is not installed!"
-
-    try:
-        import cv2
-    except:
-        return "No module cv2! cv2 is not installed!"
-
-
+def answer(weight_path: str, img_path: str) -> str:
     img = Image.open(img_path)
     img_array = np.array(img)
 
     resized_image = cv2.resize(img_array, (640, 640))
+
+    # gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 
     try:
         model = YOLO(weight_path)
     except:
         return "Weights file is not found!"
 
-    results = model.predict(resized_image)
-    names = model.names
-
     try:
         results = model.predict(resized_image)
         names = model.names
-
     except FileNotFoundError:
         return "Image file is not found!"
 
-    finally:
-        return names[int(results[0].boxes.cls[0])]
+    try:
+        rus_names = {
+            'CS120.01.413': 'CS120.01.413',
+            'CS120.07.442': 'CS120.07.442',
+            'CS150.01.427-01': 'CS150.01.427-01',
+            'SU80.01.426': 'SU80.01.426',
+            'SU80.10.409A': 'SU80.10.409A',
+            'SU160.00.404': 'SU160.00.404',
+            '3BT86_103K_02': 'ЗВТ86.103К-02',
+            'SBM_37_060': 'СВМ.37.060',
+            'SVM.37.060A': 'СВМ.37.060А',
+            'SVP_120_00_060': 'СВП-120.00.060',
+            'SVP120_42_020': 'СВП120.42.020',
+            'SVP120_42_030': 'СВП120.42.030',
+            'SK_20_01_01_01_406': 'СК20.01.01.01.406',
+            'SK_20_01_01_02_402': 'СК20.01.01.02.402',
+            'SK30.01.01.02.402': 'СК30.01.01.02.402',
+            'SK30_01_01_03_403': 'СК30.01.01.03.403',
+            'SK_50_01_01_404': 'СК50.01.01.404',
+            'SK_50_02_01_411': 'СК50.02.01.411',
+            'SPO_250_14_190': 'СПО250.14.190',
+            '-.37.060': 'СВМ.37.060',
+            '-.37.060-': 'СВМ.37.060А',
+            '-120.00.060': 'СВП-120.00.060',
+            '-120.42.020': 'СВП120.42.020',
+            '-120.42.030': 'СВП120.42.030',
+            '-20.01.01.01.406': 'СК20.01.01.01.406',
+            '-20.01.01.02.402': 'СК20.01.01.02.402',
+            '-250.14.190': 'СПО250.14.190',
+            '-30.01.01.02.402': 'СК30.01.01.02.402',
+            '-30.01.01.03.403': 'СК30.01.01.03.403',
+            '-50.01.01.404': 'СК50.01.01.404',
+            '-50.02.01.411': 'СК50.02.01.411',
+            '-86.103-02': 'ЗВТ86.103К-02',
+        }
+        name = names[int(results[0].boxes.cls[0])]
+        if name in rus_names:
+            res = rus_names[name]
+        else:
+            res = name
+        return res
+    except IndexError:
+        return "не обнаружено"
 
 
 @application.route("/check_img", methods=["POST", "GET"])
@@ -69,21 +89,21 @@ def check_img():
         urllib.request.urlretrieve(request.form["file"], "tmp.png")
     else:
         request.files['file'].save("tmp.png")
-    # print(answer("C:\\2023\\Hacathon\\hak 22-24.09 web view\\model\\model_1.pt", "C:\\2023\\Hacathon\\hak 22-24.09 web view\\tmp.png"))
+
+    if True:  # Сжатие изображение ДЕЛАЕТ ЕГО СИНИМ
+        img = Image.open("tmp.png")
+        img_array = np.array(img)
+        resized_image = cv2.resize(img_array, (640, 640))
+        cv2.imwrite(f'tmp.png', resized_image)
 
     # сюда магию проверки картинки
+    a = answer("model\\model_1.pt", "tmp.png")
 
-    response = application.response_class(
-        response=json.dumps({"type": "RC101.01.01"}),
-        status=300,
-        mimetype='application/json'
-    )
     pil_img = Image.open('tmp.png', mode='r')  # reads the PIL image
     byte_arr = io.BytesIO()
     pil_img.save(byte_arr, format='PNG')  # convert the PIL image to byte array
     encoded_img = encodebytes(byte_arr.getvalue()).decode('ascii')  # encode as base64
-    # print(data)
-    return jsonify(type="RC101.01.01", img=encoded_img)
+    return jsonify(type=a, img=encoded_img)
 
 
 @application.route("/test", methods=["POST", "GET"])
--- static/js/dropbox.js
+++ static/js/dropbox.js
@@ -9,30 +9,35 @@ function drop_handler(ev) {
     let dt = ev.dataTransfer
     let files = dt.files
 
-    Array.prototype.forEach.call(dt.files,(file) => {
-        var formData = new FormData()
-        formData.append('file', file)
-        formData.append("rjkz", "RJKZAVR")
+    for (let i = 0; i < dt.files.length; i++) {
+        const a = (file) => {
+            var formData = new FormData()
+            formData.append('file', file)
+            formData.append("rjkz", "RJKZAVR")
 
-        console.log(formData.file, "ADSADd")
+            console.log(formData.file, "ADSADd")
 
-        fetch("/check_img", {
-            method: 'POST',
-            body: formData
-        }).then((response) => response.json())
-        .then((data) => {
-            let images = document.getElementById("img")
-            let div = document.createElement("div")
-            div.classList.add("checked__block")
-            let image = document.createElement('img')
-            image.src="data:image/png;base64,"+data.img
-            image.alt=data.type
-            div.append(image)
-            let p = document.createElement('p')
-            p.innerText = data.type
-            div.append(p)
-            images.append(div)
-            console.log("GET ANSWER")
-        })
-    })
+            fetch("/check_img", {
+                method: 'POST',
+                body: formData
+            }).then((response) => response.json())
+                .then((data) => {
+                    let images = document.getElementById("img")
+                    let div = document.createElement("div")
+                    div.classList.add("checked__block")
+                    let image = document.createElement('img')
+                    image.src = "data:image/png;base64," + data.img
+                    image.alt = data.type
+                    div.append(image)
+                    let p = document.createElement('p')
+                    p.innerText = data.type
+                    div.append(p)
+                    images.append(div)
+                    console.log("GET ANSWER")
+                })
+        }
+
+        setTimeout(a, 7000*i, dt.files[i]);
+
+    }
 }
